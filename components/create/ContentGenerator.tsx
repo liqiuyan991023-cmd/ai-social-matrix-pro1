e493348135003d03207d32a5524e780910772f15
liqiuyan991023-cmd <liqiuyan991023@gmail.com>
2026-04-02 13:33:11 +0800

    15

--- components/create/ContentGenerator.tsx
+++ components/create/ContentGenerator.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { TopicRecommendation } from "@/lib/types";
+import { TopicRecommendation } from "../../lib/types/index";
 
 interface ContentGeneratorProps {
   userId: string;
